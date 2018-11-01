4d5e4d6442b51acad000a9e4a816a49f3b74d1be
JaimeGuti <jaime.m.guti@gmail.com>
2018-11-01 13:59:33 +0100

    Añadido al servidor el método register2json, y la hora de expiración del usuario

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import json
+import time
 
 
 PORT = int(sys.argv[1])
@@ -21,17 +23,26 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         line = self.rfile.read()
+        exp = float(line.decode('utf-8').split()[-1])
+        tm = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() + exp))
+        info = {"address": self.client_address[0], "expires": tm}
         if line.decode('utf-8').split(' ')[0] == 'REGISTER':
             name_client = line.decode('utf-8').split(' ')[1][4:]
-            self.clients[name_client] = self.client_address[0]
+            self.clients[name_client] = info
+            self.register2json()
             if line.decode('utf-8').split(' ')[3][-1] == 'Expires':
-                expires = int(line.decode('utf-8').split(' ')[5])
+                expires = int(line.decode('utf-8').split(' ')[-1])
                 if str(expires) == '0':
-                    del self.clientes[name_client]
+                    del self.clients[name_client]
                     print(b"SIP/2.0 200 OK\r\n\r\n")
 
         print(line.decode('utf-8'))
         print(self.clients)
+
+    def register2json(self):
+        json_file = open('registered.json', 'w')
+        json.dump(self.clients, json_file)
+
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
